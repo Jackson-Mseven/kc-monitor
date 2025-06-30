3c8469532b9de6bcd29db1ee3dc434bf4795ad59
kincy <bill714@foxmail.com>
2025-06-30 22:41:11 +0800

    fix(dashboard): 修改用户获取团队成员接口路径

--- packages/dashboard/src/hooks/swr/useTeamMembers.ts
+++ packages/dashboard/src/hooks/swr/useTeamMembers.ts
@@ -3,7 +3,7 @@ import { User } from '@kc-monitor/shared'
 import useSWR from 'swr'
 
 const useTeamMembers = () => {
-  const { data, isLoading, error } = useSWR('/team/me/members', getFetcher)
+  const { data, isLoading, error } = useSWR('/user/me/team/members', getFetcher)
   return {
     teamMembers: data?.data as Omit<User, 'password'>[],
     isLoading,
