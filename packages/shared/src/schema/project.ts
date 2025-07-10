c40949ff12b5ddb1d1f8ff0f2d555aca75345679
kincy <bill714@foxmail.com>
2025-07-10 14:07:13 +0800

    feat(backend): 完善项目模块

--- packages/shared/src/schema/project.ts
+++ packages/shared/src/schema/project.ts
@@ -2,13 +2,15 @@ import { z } from 'zod'
 import { generateNameSchema, generateSlugSchema, IDSchema } from './common'
 
 export const ProjectNameSchema = generateNameSchema('项目')
-
 export const ProjectSlugSchema = generateSlugSchema('项目')
 
 export const ProjectSchema = z.object({
   team_id: z.number({ message: '团队ID不能为空' }),
   name: ProjectNameSchema,
   slug: ProjectSlugSchema,
+  description: z.string().optional(),
+  versions: z.array(z.string()),
+  platform: z.string(),
 })
 
 export const ProjectParamsSchema = z.object({
